d8e2ee7a70f657c904aa30d7bb5ef58e92928715
David Khauan <thalsmatsumira@gmail.com>
2024-06-18 09:15:03 -0300

    primeiro commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Produto 1',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: "Pending"
+    },
+    {
+        productName: 'Produto 2',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: "Declined"
+    },
+    {
+        productName: 'Produto 3',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: "Active"
+    }
+]
